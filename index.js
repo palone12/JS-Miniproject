dac1a4a958002813f60daeed865f6fa3de3129fa
Pranay Alone <palone12>
2022-06-29 00:17:49 +0530

    Javascript Added

--- index.js
+++ index.js
@@ -12,4 +12,20 @@ window.onclick = function (event) {
         document.getElementById('courses_id')
             .style.display = "none";
     }
-}   
+}
+function show_list2() {
+    var courses = document.getElementById("courses_id2");
+
+    if (courses.style.display == "block") {
+        courses.style.display = "none";
+    } else {
+        courses.style.display = "block";
+    }
+}
+window.onclick = function (event) {
+    if (!event.target.matches('.dropdown_button')) {
+        document.getElementById('courses_id')
+            .style.display = "none";
+    }
+}
+   
